013758b73b701af2916ea31a463904e04f15ceeb
Beatriz <balmeida82282@gmail.com>
2026-03-09 17:43:20 -0300

    logout

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 db = SQLAlchemy ()
+login_manager = LoginManager()
 app = Flask(__name__)
 app.config[ 'SQLALCHEMY_DATABASE_URI'] = "sqlite:///mercado.db"
 app.config["SECRET_KEY"] = 'e0bedac370ad4ba97cc7ab58'
 db.init_app(app)
 bcrypt=Bcrypt(app)
+login_manager.init_app(app)
 
 from mercado import routes
--- mercado/routes.py
+++ mercado/routes.py
@@ -1,8 +1,9 @@
 from mercado import app
-from flask import render_template,redirect,url_for,flash
-from mercado.models import Item,User
-from mercado.forms import CadastroForm, LoginForm
+from flask import render_template, redirect, url_for, flash
+from mercado.models import Item, User
+from mercado.forms import CadastroForm , LoginForm
 from mercado import db
+from flask_login import login_user , logout_user
 
 @app.route('/')
 def page_home():
@@ -22,16 +23,30 @@ def page_cadastro():
             email = form.email.data,
             senhacrip = form.senha1.data
         )
-
         db.session.add(usuario)
         db.session.commit()
         return redirect(url_for('page_produto'))
+
     if form.errors != {}:
         for err in form.errors.values():
-            flash(f'Erro ao cadastrar usuário {err} ',category="danger")
-    return render_template("cadastro.html", form=form)
+            flash(f'Erro ao cadastrar usuário {err}', category="danger")
+    return render_template("cadastro.html",form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET','POST'])
 def page_login():
-    form =LoginForm()
-    return render_template('login.html',form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        usuario_logado = User.query.filter_by(usuario = form.usuario.data).first()
+        if usuario_logado and usuario_logado.converte_senha(senha_texto_claro = form.senha1.data):
+            login_user(usuario_logado)
+            flash(f'Sucesso seu login é: {usuario_logado.usuario}', category='success')
+            return redirect(url_for('page_produto'))
+        else:
+            flash(f'Usuário ou senha estão incorretas! Tente novamente', category='danger')
+    return render_template('login.html', form = form)
+
+@app.route('/logout')
+def page_logout():
+    logout_user()
+    flash("Você fez o logout", category="info")
+    return redirect(url_for("page_home"))
